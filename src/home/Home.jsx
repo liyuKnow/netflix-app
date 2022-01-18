5a1a1dda340a40db53ff972cfb716be8740efb25
liyuKnow <kidus2011@gmail.com>
2022-01-18 04:54:28 -0800

    navbar

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,9 +1,15 @@
+import Navbar from "../components/navbar/Navbar";
 import "./Home.scss";
 
 const Home = () => {
     return (
         <div className="home">
-            <h1>Home</h1>
+            <Navbar />
+            <img
+                width="100%"
+                src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+            />
         </div>
     )
 }
