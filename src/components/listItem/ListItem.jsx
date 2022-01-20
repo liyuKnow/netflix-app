4bfaa7c7bcf6a14cf9cbce893ad84e07073427ed
liyuKnow <kidus2011@gmail.com>
2022-01-20 12:44:37 -0800

    movie list slider and movie watching page

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,19 +1,43 @@
+import React, { useState } from 'react';
 import { Add, LinkOutlined, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from '@material-ui/icons';
-import React from 'react';
 import './ListItem.scss';
 
-const ListItem = () => {
+const ListItem = ({ index }) => {
+    const [isHovered, setIsHovered] = useState(false);
+    const trailer =
+        "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
     return (
-        <div className='list-item'>
+        <div
+            className='list-item'
+            style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+            onMouseEnter={() => { setIsHovered(true) }}
+            onMouseLeave={() => { setIsHovered(false) }}
+        >
             <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" alt="" />
-            <div className="itemInfo">
-                <div className="icons">
-                    <PlayArrow />
-                    <Add />
-                    <ThumbUpAltOutlined />
-                    <ThumbDownAltOutlined />
-                </div>
-            </div>
+            {isHovered && (
+                <>
+                    <video src={trailer} autoplay="true" loop />
+                    <div className="itemInfo">
+                        <div className="icons">
+                            <PlayArrow className='icon' />
+                            <Add className='icon' />
+                            <ThumbUpAltOutlined className='icon' />
+                            <ThumbDownAltOutlined className='icon' />
+                        </div>
+                        <div className="itemInfoTop">
+                            <span>1 hr 23 mins </span>
+                            <span className='ageLimit'>+16</span>
+                            <span>1999</span>
+                        </div>
+                        <div className="description">
+                            lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        </div>
+                        <div className="genre">Action</div>
+                    </div>
+                </>
+            )}
+
         </div>
     );
 };
---
+++ src/pages/watch/Watch.jsx
@@ -0,0 +1,23 @@
+import { ArrowBackOutlined } from '@material-ui/icons';
+import React from 'react';
+import './Watch.scss';
+
+const Watch = () => {
+    return (
+        <div className='watch'>
+            <div className="back">
+                <ArrowBackOutlined />
+                <span className="text">Home</span>
+            </div>
+            <video
+                src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                className="video"
+                autoplay
+                progress
+                controls
+            />
+        </div>
+    );
+};
+
+export default Watch;
